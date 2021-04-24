53e67b837d8f77009f30f283ca5d46b37ce1f53d
Noah Crocker <vertigoetrex@users.noreply.github.com>
2021-04-24 07:18:46 -0500

    describe

--- overland-lambda-rollup/overland_lambda_rollup/main.py
+++ overland-lambda-rollup/overland_lambda_rollup/main.py
@@ -2,15 +2,17 @@ from .dlsToObjs import loadAllFiles
 from pathlib import Path
 import pandas as pd
 
+
 def main():
     print("loading all files")
     entries = loadAllFiles(Path('./dl'))
     print(f"got {len(entries)} entries")
     print(entries[0])
     df = pd.DataFrame.from_records(entries)
+    df.sort_values('ts', ignore_index=True, inplace=True)
     print(df)
     print(df.dtypes)
-
+    print(df.describe())
 
 
 if __name__ == '__main__':
