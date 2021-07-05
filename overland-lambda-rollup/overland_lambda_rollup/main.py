6757b0c833fb993ef0e0b0d4ca1a034f94a71469
Noah Crocker <vertigoetrex@users.noreply.github.com>
2021-07-04 22:51:02 -0700

    process media

--- overland-lambda-rollup/overland_lambda_rollup/main.py
+++ overland-lambda-rollup/overland_lambda_rollup/main.py
@@ -5,12 +5,59 @@ import logging
 import os
 import pyarrow as pa
 from tqdm import tqdm
+import click
+import filetype
+from rich.logging import RichHandler
 
-# logging.setLevel(logging.DEBUG)
-logging.basicConfig(level=logging.INFO)
+FORMAT = "%(message)s"
+logging.basicConfig(
+    level=logging.INFO,
+    handlers=[RichHandler(rich_tracebacks=True)],
+    format=FORMAT,
+    datefmt="[%X]",
+)
 
 
-def main():
+
+@click.command()
+@click.argument('media-tree-dir')
+def handle_media(media_tree_dir):
+    """
+    Enumerates media locally or on S3, converts to p-dash, creates timeline, and optionally uploads to S3
+    """
+    logging.info("enumerating all media")
+
+    source_path = Path(media_tree_dir)
+    other_types = {}
+    videos = []
+    images = []
+    total = 0
+    for file in (f for f in source_path.glob("**/*") if f.is_file()):
+        total += 1
+        file_type = filetype.guess(file.as_posix())
+        if file_type == None:
+            other_types["unknown"] = other_types.get("unknown", 0) + 1
+            logging.info(f"UNKNOWN {file}")
+        elif file_type.mime in {'image/tiff', 'image/heic', 'image/jpeg', 'image/png', 'image/x-canon-cr2'}:
+            images.append(file)
+        elif file_type.mime in {'video/quicktime', 'video/mp4'}:
+            videos.append(file)
+        else:
+            other_types[file_type.mime] = other_types.get(file_type.mime, 0) + 1
+
+    for k, v in other_types.items():
+        logging.info(f"{k}: {v}")
+
+    logging.info("")
+    logging.info(f"videos, {len(videos)}")
+    logging.info(f"images {len(images)}")
+
+
+
+
+
+
+def location_data_to_arrow():
     logging.info("loading all files")
     entries = loadAllFiles(Path('./dl'))
     logging.info(f"got {len(entries)} entries")
